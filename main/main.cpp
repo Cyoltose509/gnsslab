da2863618e046d2fa1fbbbd5356fbde9f618303e
cyoltose <bilibilicookie@outlook.com>
2026-04-08 14:47:25 +0800

    feat(main): add output file writing functionality

--- main/main.cpp
+++ main/main.cpp
@@ -1,11 +1,19 @@
 #include <sstream>
 #include<cmath>
 #include <string>
+#include  <fstream>
 #include"OEM7Reader.h"
 
 
 int main() {
     const std::string filename = "NovatelOEM20211114-01.log";
+    std::ofstream outFile("output.txt");
+    if (!outFile.is_open()) {
+        throw InvalidRequest("Could not open output file.");
+        return 1;
+    }
+
+
 
     const auto ct = CivilTime2CommonTime(CivilTime(2021, 11, 14, 7, 25, 0.00001788));
 
@@ -14,8 +22,8 @@ int main() {
     for (int i = 1; i < 100; i++) {
         auto data = oem7.readOne();
         if (data) {
-            PVT pvt= data->svPVT(ct);
-            cout << data->name() << " "
+            PVT pvt = data->svPVT(ct);
+            outFile << data->name() << " "
                     << std::fixed << std::setprecision(6)
                     << pvt.p[0] * 0.001 << " " << pvt.p[1] * 0.001 << " " << pvt.p[2] * 0.001 << " "
                     << std::fixed << std::setprecision(3)
